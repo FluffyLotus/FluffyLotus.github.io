6df11863c0b9f2fa9c7b8e01a169406ccb66a656
FluffyLotus <33723671+FluffyLotus@users.noreply.github.com>
2021-08-31 13:49:49 -0400

    Update uiApp.js

--- a/js/ui/uiApp.js
+++ a/js/ui/uiApp.js
@@ -4,9 +4,9 @@
     //resources[RESOURCE_STONE].amount = 40;
     //resources[RESOURCE_WOOD].amount = 40;
 
-    resources[RESOURCE_STONE].addAmount(20000);
-    resources[RESOURCE_WOOD].addAmount(20000);
-    resources[RESOURCE_PLANK].addAmount(20000);
+    //resources[RESOURCE_STONE].addAmount(20000);
+    //resources[RESOURCE_WOOD].addAmount(20000);
+    //resources[RESOURCE_PLANK].addAmount(20000);
 
     uiInitCanvas();
 
